c8d575e6f565a7b07bd1bf9b4789ad48a55186a8
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 14:40:52 +0530

    Added: UC_3_CheckWhetherLinesAreEqualOrGreaterOrSmallerUsingCompareTo

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -10,6 +10,9 @@ public class LineComparison {
 
         LineComparison UC_2 = new LineComparison();
         UC_2.UC_2_CheckEqualityOfTwoLines();
+
+        LineComparison UC_3 = new LineComparison();
+        UC_3.UC_3_CheckWhetherLinesAreEqualOrGreaterOrSmallerUsingCompareTo();
     }
     public void UC_1_LineLengthCalculator(){
         Scanner sc = new Scanner(System.in);
@@ -72,6 +75,52 @@ public class LineComparison {
         else
         {
             System.out.println("Lines are not equal");
+        }
+    }
+    public void UC_3_CheckWhetherLinesAreEqualOrGreaterOrSmallerUsingCompareTo(){
+        Scanner sc = new Scanner(System.in);
+        // Input for the first point
+        System.out.println("Enter first line details : ");
+        System.out.print("Enter x1: ");
+        double firstLine_x1 = sc.nextDouble();
+        System.out.print("Enter y1: ");
+        double firstLine_y1 = sc.nextDouble();
+        // Input for the second point
+        System.out.print("Enter x2: ");
+        double firstLine_x2 = sc.nextDouble();
+        System.out.print("Enter y2: ");
+        double firstLine_y2 = sc.nextDouble();
+        // Calculate the length of the first line
+        double firstLineDeltaX = firstLine_x2 - firstLine_x1;
+        double firstLineDeltaY = firstLine_y2 - firstLine_y1;
+        double firstLineLength = Math.sqrt(firstLineDeltaX * firstLineDeltaX + firstLineDeltaY * firstLineDeltaY);
+        System.out.println("The length of the line is: " + firstLineLength);
+        System.out.println("---------------------------------");
+        // Second Line inputs
+        System.out.println("Enter second line details : ");
+        System.out.print("Enter x1: ");
+        double secondLine_x1 = sc.nextDouble();
+        System.out.print("Enter y1: ");
+        double secondLine_y1 = sc.nextDouble();
+        // Input for the second point
+        System.out.print("Enter x2: ");
+        double secondLine_x2 = sc.nextDouble();
+        System.out.print("Enter y2: ");
+        double secondLine_y2 = sc.nextDouble();
+        // Calculate the length of the first line
+        double secondLineDeltaX = secondLine_x2 - secondLine_x1;
+        double secondLineDeltaY = secondLine_y2 - secondLine_y1;
+        double secondLineLength = Math.sqrt(secondLineDeltaX * secondLineDeltaX + secondLineDeltaY * secondLineDeltaY);
+        String firstLineLengthString = Double.toString(firstLineLength);
+        String secondLineLengthString = Double.toString(secondLineLength);
+        if(firstLineLengthString.compareTo(secondLineLengthString)==0) {
+            System.out.println("Both lines have same length");
+        }
+        else if(firstLineLengthString.compareTo(secondLineLengthString)>0) {
+            System.out.println("First line is greater than second line");
+        }
+        else {
+            System.out.println("First line is smaller than second line");
         }
     }
 }
