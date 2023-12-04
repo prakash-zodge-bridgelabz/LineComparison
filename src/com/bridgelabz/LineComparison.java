972a375869a9181e844d02f3d4cca15c70622208
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-04 13:42:00 +0530

    Added: Line length calculator by UC_1_branch

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,7 +1,35 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        LineComparison UC_1 = new LineComparison();
+        UC_1.UC_1_LineLengthCalculator();
+    }
+    public void UC_1_LineLengthCalculator(){
+        Scanner sc = new Scanner(System.in);
+
+        // Input for the first point
+        System.out.print("Enter x1: ");
+        double x1 = sc.nextDouble();
+        System.out.print("Enter y1: ");
+        double y1 = sc.nextDouble();
+
+        // Input for the second point
+        System.out.print("Enter x2: ");
+        double x2 = sc.nextDouble();
+        System.out.print("Enter y2: ");
+        double y2 = sc.nextDouble();
+
+        // Calculate the length of the line
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+        double length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        // Display the result
+        System.out.println("The length of the line is: " + length);
+
     }
 }
